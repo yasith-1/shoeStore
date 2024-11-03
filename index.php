80aa60bb2aaf38c178892374add5d871e3b14cf5
Yasith <141606159+yasith-1@users.noreply.github.com>
2024-11-03 23:29:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- link css file -->
-    <link rel="stylesheet" href="/.style.css">
+    <link rel="stylesheet" href="style.css">
     <!-- link css file -->
 
     <!--link Bootstrap -->
@@ -21,8 +21,8 @@
     <!-- Navbar start  ---------------------------------------------------------------------------------------------------------------------------->
     <section>
 
-        <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark">
-            <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+            <div class="container-fluid ">
                 <a class="navbar-brand" href="#">Navbar</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
